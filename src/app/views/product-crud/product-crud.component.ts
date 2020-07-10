e522b5bf7ee1ccc72f3b2b81df4a28dbff175a1d
rodrigoalien <rodrigodacunhaalmeida@gmail.com>
2020-07-10 08:58:29 -0300

    Functions Create product and SnackBar.

--- src/app/views/product-crud/product-crud.component.ts
+++ src/app/views/product-crud/product-crud.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-crud',
@@ -14,7 +14,7 @@ export class ProductCrudComponent implements OnInit {
   }
 
   navigateToProductCreate(): void {
-    this.router.navigate(['products/create']);
+    this.router.navigate(['/products/create']);
     
   }
 
